f47bcd8b09539b3c0b9be0d4aa718e09e9fabef4
Eduardo Serrano <eduardoserrano8591@gmail.com>
2022-03-03 22:40:25 -0500

    Add review to restaurant

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
-  resources :restaurants, only: [:show]
+  get 'reviews/create'
+  resources :restaurants, only: %i[show] do
+    resources :reviews, only: :create
+  end
+
   root to: 'restaurants#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
